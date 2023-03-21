0b25d10225e6b67232c2121b696e815310927d52
longo2002 <lombardo.fra2002@gmail.com>
2023-03-21 13:40:53 +0100

    Attempt 1

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Censura</title>
+</head>
+
+<body>
+
+  <form action="./censura.php" method="GET">
+    <input require type="text" name="parola" placeholder="Scrivi una parola da censurare">
+    <textarea required name="paragrafo" cols="30" rows="10" placeholder="Incolla il testo da censurare"></textarea>
+    <input type="submit" value="Invia">
+  </form>
+
+</body>
+
+</html>
